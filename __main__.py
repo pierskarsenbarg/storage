d8ad80f89a17ee3f2967a76d6c8907f28c87bf09
Piers Karsenbarg <piers@pulumi.com>
2025-04-22 13:13:18 +0100

    First commit

---
+++ __main__.py
@@ -0,0 +1,5 @@
+from pulumi.provider.experimental import component_provider_host
+from storage import Storage, StorageArgs
+
+if __name__ == "__main__":
+    component_provider_host(name="storage-component", components=[Storage])
