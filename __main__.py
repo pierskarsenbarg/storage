6f9c87e0d2d57b0728d654c71dce3e5e9d09e6d9
Piers Karsenbarg <piers@pulumi.com>
2025-04-22 14:15:15 +0100

    renamed component

--- __main__.py
+++ __main__.py
@@ -2,4 +2,4 @@ from pulumi.provider.experimental import component_provider_host
 from storage import Storage, StorageArgs
 
 if __name__ == "__main__":
-    component_provider_host(name="storage-component", components=[Storage])
+    component_provider_host(name="storage", components=[Storage])
