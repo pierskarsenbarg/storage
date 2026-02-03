1aaff5b3eb55d9d448488eb582c3c0c6b32dd596
Piers Karsenbarg <piers@pulumi.com>
2026-02-03 11:45:07 +0000

    Removed StorageArgs reference from __main__.py

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 from pulumi.provider.experimental import component_provider_host
-from storage import Storage, StorageArgs
+from storage import Storage
 
 if __name__ == "__main__":
     component_provider_host(name="storage", components=[Storage])
